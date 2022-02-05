6213691a7e577dac5793f82a441ac1216c184ec9
Adam <44466509+RP098@users.noreply.github.com>
2022-02-05 15:41:15 +0100

    AlgorytmyPoczątkiC_Sharp
    -SORTOWANIE BOMBELKOWE
    -SORTOWANIE PRZEZ WYBÓR
    -SORTOWANIE PRZEZ WSTAWIANIE
    -SORTOWANIE PRZEZ ZLICZANIE
    -CIĄG FIBONACIEGO{iteracyjnie,rekurencyjnie}
    -ALGORYTM EUKLIDESA
    -sORTOWANIE MERGE SORT
    -NAJWIEKSZY WSPÓLNY PODCIĄG
    -KOLEJKA
    -STOS
    -DRZEWO BST PUSH ZNAJDZ RODZICA
    -PRE ORDER
    -IN ORDER
    -POST ORDER
    -KOPIEC
    -SORTOWANIE PRZEZ KOPCOWANIE
    -DRZEWO BINARNE
    -GRAF
    -PRZESZUKIWANIE WSZERZ
    -PRZESZUKIWANIE W GŁĄB
    -GŁĘBOKOŚĆ GRAFU
    -ALGORYTM DIJSKTRY
    -ALGORYTM KRUSKALA
    -ALGORYTM PRIMA

---
+++ AlgorytmyUczelnia2019-2020/Drzewo_Konsolowa/BinarySearchTreeIStosIKolejka/WspolneInstrukcjeStosuIKolejki.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stos_kolejka
+{
+    abstract class SD
+    {
+
+        protected int[] dane;
+        public int Lenght
+        {
+            get;
+            protected set;
+        }
+
+
+
+        public SD()
+        {
+            this.Lenght = 0;
+            this.dane = new int[this.Lenght];
+        }
+
+        public int this[int index]
+        {
+            get { return this.dane[index]; }
+            set { this.dane[index] = value; }
+        }
+
+
+
+        public void Push(int element)
+        {
+            this.Lenght++;
+            int[] tmp = this.Przepisz(this.dane);
+
+            tmp[this.Lenght - 1] = element;
+            this.dane = tmp;
+
+
+        }
+        protected int[] Przepisz(int[] stara)
+        {
+
+            int[] nowa = new int[stara.Length + 1];
+            for (int i = 0; i < stara.Length; i++)
+            {
+                nowa[i] = stara[i];
+            }
+            return nowa;
+        }
+    }
+}
---
+++ AlgorytmyUczelnia2019-2020/Drzewo_Konsolowa/BinarySearchTreeIStosIKolejka/stos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stos_kolejka
+{
+    class stos : SD, IAiSD
+    {
+
+        public int Pop()
+        {
+            if (Lenght == 0)
+            {
+
+                Console.WriteLine("Na stosie nie ma nikogo nie ma co podpalac");
+                return 0;
+
+            }
+            Lenght--;
+            int elementKtoryZostalUsunietyZeStosu = this.PrzepiszStos(this.dane);
+            return elementKtoryZostalUsunietyZeStosu;
+
+        }
+        protected int PrzepiszStos(int[] staraStos)
+        {
+            int elementStosuUsuwany;
+            int[] nowyStos = new int[staraStos.Length - 1];
+            elementStosuUsuwany = staraStos[staraStos.Length - 1];
+            for (int i = 0; i <= staraStos.Length - 2; i++)
+            {
+                nowyStos[i] = staraStos[i];
+            }
+            return elementStosuUsuwany;
+        }
+
+    }
+}
