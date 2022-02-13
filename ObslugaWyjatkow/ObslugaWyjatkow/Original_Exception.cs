0b2a564e871316ccceba6e96a87747131214c740
Adam <44466509+RP098@users.noreply.github.com>
2022-02-13 22:59:14 +0100

    Rozdział 7 obsługa wyjątków.

---
+++ ObslugaWyjatkow/ObslugaWyjatkow/Original_Exception.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObslugaWyjatkow
+{
+    class Original_Exception : ApplicationException
+    {
+        private string ExceptionMessage;
+        public DateTime dateTimeException {get; set;}
+        public string Cause_Error { get; set; }
+        public Original_Exception() { }
+        public Original_Exception(string ExceptionMessage,DateTime dateTimeException, string Cause_Error)
+        {
+            this.ExceptionMessage = ExceptionMessage;
+            this.dateTimeException = dateTimeException;
+            this.Cause_Error = Cause_Error;
+        }
+
+        public override string Message {
+            get
+            {
+                return string.Format($"You must in next time {ExceptionMessage} \nYour Exception occured because {Cause_Error}\n Exception has been throw {dateTimeException} ");
+            }
+        
+        }
+
+
+
+    }
+}
