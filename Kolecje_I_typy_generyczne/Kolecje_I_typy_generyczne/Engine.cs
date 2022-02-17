7e48473a70feb2bf58792f651ac2e67a367a61a3
Adam <44466509+RP098@users.noreply.github.com>
2022-02-17 22:09:33 +0100

    Kolekcje i typy generyczne.
    Utrwalenie wiedzy o kolekcjach i typach generycznych, metodach generycznych, słowie kluczowym default oraz słowie kluczowyn where.

---
+++ Kolecje_I_typy_generyczne/Kolecje_I_typy_generyczne/Engine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Kolecje_I_typy_generyczne
+{
+    class Engine:IComparer<Engine>
+    {
+        DateTimeOffset DateTimeOffset = new DateTimeOffset(DateTime.Now);
+           
+        public int power_hp { get; set; }
+        public int torque { get; set; }
+        public double efficiency { get; set; }
+        public bool engine_on_of
+        {
+            get; set;
+        }
+        public Engine()
+        {
+            long seed = DateTimeOffset.ToUnixTimeSeconds();
+      
+            Random random = new Random((int)Math.Abs(seed)*DateTime.Now.Millisecond);
+            power_hp = random.Next(80,160);
+            torque = random.Next(170, 250); ;
+            efficiency = random.Next(35, 40); ;
+            engine_on_of = false;
+        }
+        public override string ToString()
+        {
+            return $"PowerHP = {this.power_hp} Torque = {this.torque} Efficiency = {this.efficiency} Engine ON? = {engine_on_of}";
+        }
+
+        public int Compare([AllowNull] Engine x, [AllowNull] Engine y)
+        {
+            if (x.power_hp > y.power_hp)
+                return 1;
+            else if (x.power_hp == y.power_hp)
+                return 0;
+            else
+                return -1;
+        }
+    }
+}
