5bdaa27b7b5802a8f0aeaa177db27669f6900d53
Adam <44466509+RP098@users.noreply.github.com>
2022-03-21 18:21:05 +0100

    Kolejny element związany z ado net

---
+++ Ksiazka/ado_net_2_warstwaBezpolaczeniowa/AdoNet_W_winforms/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace AdoNet_W_winforms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCarInventory = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.textBoxView = new System.Windows.Forms.TextBox();
+            this.textBoxDelete = new System.Windows.Forms.TextBox();
+            this.groupBoxDeleteCarID = new System.Windows.Forms.GroupBox();
+            this.groupBoxViewData = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCarInventory)).BeginInit();
+            this.groupBoxDeleteCarID.SuspendLayout();
+            this.groupBoxViewData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCarInventory
+            // 
+            this.dataGridViewCarInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCarInventory.Location = new System.Drawing.Point(12, 40);
+            this.dataGridViewCarInventory.Name = "dataGridViewCarInventory";
+            this.dataGridViewCarInventory.Size = new System.Drawing.Size(463, 313);
+            this.dataGridViewCarInventory.TabIndex = 0;
+            this.dataGridViewCarInventory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCarInventory_CellContentClick);
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.Location = new System.Drawing.Point(119, 31);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(75, 23);
+            this.button_Delete.TabIndex = 1;
+            this.button_Delete.Text = "Delete";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(119, 33);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(75, 23);
+            this.buttonView.TabIndex = 2;
+            this.buttonView.Text = "View";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // textBoxView
+            // 
+            this.textBoxView.Location = new System.Drawing.Point(6, 33);
+            this.textBoxView.Name = "textBoxView";
+            this.textBoxView.Size = new System.Drawing.Size(100, 20);
+            this.textBoxView.TabIndex = 3;
+            // 
+            // textBoxDelete
+            // 
+            this.textBoxDelete.Location = new System.Drawing.Point(6, 31);
+            this.textBoxDelete.Name = "textBoxDelete";
+            this.textBoxDelete.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDelete.TabIndex = 4;
+            // 
+            // groupBoxDeleteCarID
+            // 
+            this.groupBoxDeleteCarID.Controls.Add(this.button_Delete);
+            this.groupBoxDeleteCarID.Controls.Add(this.textBoxDelete);
+            this.groupBoxDeleteCarID.Location = new System.Drawing.Point(27, 359);
+            this.groupBoxDeleteCarID.Name = "groupBoxDeleteCarID";
+            this.groupBoxDeleteCarID.Size = new System.Drawing.Size(200, 79);
+            this.groupBoxDeleteCarID.TabIndex = 5;
+            this.groupBoxDeleteCarID.TabStop = false;
+            this.groupBoxDeleteCarID.Text = "Enter ID of Car to Delete";
+            // 
+            // groupBoxViewData
+            // 
+            this.groupBoxViewData.Controls.Add(this.textBoxView);
+            this.groupBoxViewData.Controls.Add(this.buttonView);
+            this.groupBoxViewData.Location = new System.Drawing.Point(246, 359);
+            this.groupBoxViewData.Name = "groupBoxViewData";
+            this.groupBoxViewData.Size = new System.Drawing.Size(200, 79);
+            this.groupBoxViewData.TabIndex = 6;
+            this.groupBoxViewData.TabStop = false;
+            this.groupBoxViewData.Text = "Enter Make to View";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 450);
+            this.Controls.Add(this.groupBoxViewData);
+            this.Controls.Add(this.groupBoxDeleteCarID);
+            this.Controls.Add(this.dataGridViewCarInventory);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCarInventory)).EndInit();
+            this.groupBoxDeleteCarID.ResumeLayout(false);
+            this.groupBoxDeleteCarID.PerformLayout();
+            this.groupBoxViewData.ResumeLayout(false);
+            this.groupBoxViewData.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCarInventory;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.TextBox textBoxView;
+        private System.Windows.Forms.TextBox textBoxDelete;
+        private System.Windows.Forms.GroupBox groupBoxDeleteCarID;
+        private System.Windows.Forms.GroupBox groupBoxViewData;
+    }
+}
+
---
+++ Ksiazka/ado_net_2_warstwaBezpolaczeniowa/AdoNet_W_winforms/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet_W_winforms
+{
+    public partial class MainForm : Form
+    {
+        List<Car> listCars = null;
+        DataTable inventoryTable = new DataTable();
+        public MainForm()
+        {
+           
+
+            listCars = new List<Car> 
+            { 
+            new Car{Id=1,CarName="Chucky",Make="BMW",Color="Green"},
+            new Car{Id=2,CarName="Tiny",Make="Opel",Color="White"},
+            new Car{Id=3,CarName="Ami",Make="Ford",Color="Tan"},
+            new Car{Id=4,CarName="Pain",Make="Jeep",Color="Pink"},
+            new Car{Id=5,CarName="Fred",Make="BMW",Color="Green"},
+            new Car{Id=6,CarName="Sidd",Make="BMW",Color="Black"},
+            new Car{Id=7,CarName="Mel",Make="Colt",Color="Red"},
+            new Car{Id=8,CarName="Sarah",Make="Corsa",Color="Black"} 
+            };
+            InitializeComponent();
+            CreateDataTable();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        void CreateDataTable()
+        {
+            var CarIdColumn = new DataColumn("Id", typeof(int));
+            var CarNameColumn = new DataColumn("CarName", typeof(string)) { Caption = "Car Name"};
+            var MakeColumn = new DataColumn("Make", typeof(string));
+            var ColorColumn = new DataColumn("Color", typeof(string));
+
+            inventoryTable.Columns.AddRange(new[] { CarIdColumn, CarNameColumn, MakeColumn, ColorColumn });
+            foreach (var item in listCars)
+            {
+                var newRow = inventoryTable.NewRow();
+                newRow[0] = item.Id;
+                newRow[1] = item.CarName;
+                newRow[2] = item.Make;
+                newRow[3] = item.Color;
+                inventoryTable.Rows.Add(newRow);
+                dataGridViewCarInventory.DataSource=inventoryTable;
+            }
+            
+
+
+        }
+
+        private void dataGridViewCarInventory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow[] rowToDelete = inventoryTable.Select($"Id={int.Parse(textBoxDelete.Text)}");
+                rowToDelete[0].Delete();
+                inventoryTable.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            StringBuilder filter = new StringBuilder();
+            filter.Append( $"Make='{textBoxView.Text}'");
+            DataRow [] dataRowMake = inventoryTable.Select(filter.ToString());
+            if (dataRowMake.Length == 0)
+                MessageBox.Show("Sorry, no cars...");
+            else
+            { StringBuilder dateRow = new StringBuilder();
+                for (int i = 0; i < dataRowMake.Length; i++)
+                {
+                    dateRow.Append(dataRowMake[i]["CarName"] + "\n");
+                }
+                MessageBox.Show("All element \n" + dateRow.ToString());
+
+            }
+            
+        }
+    }
+}
