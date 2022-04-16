fccbe232cdd99f4884b611b180df5c521211f4ad
Adam <adagor00673@gmail.com>
2022-04-16 22:33:42 +0200

    Dodanie biblioteki odpowiedzialnej za połączenie

---
+++ NetwiseZadanie/DownloadWebText/DownloadTxt/DownloadTextFromWeb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Configuration;
+namespace DownloadWebText
+{
+    public class DownloadTextFromWeb:IDownload
+    {
+        public string Download()
+        {
+            AppSettingsReader appSettings = new AppSettingsReader();
+            StringBuilder theCatText = new StringBuilder();
+            WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += (s, eArgs) => 
+            {
+                theCatText.Append(eArgs.Result);
+            };
+            string webURL = (string)appSettings.GetValue("EndPoint", typeof(string));
+            webClient.DownloadStringAsync(new Uri(webURL));
+            return theCatText.ToString();
+        }
+    }
+}
