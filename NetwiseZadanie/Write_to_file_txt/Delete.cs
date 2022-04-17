69d2879986a900c07d24e547524579076da15ac5
Adam <44466509+RP098@users.noreply.github.com>
2022-04-17 13:29:03 +0200

    Dodanie katalogu

---
+++ NetwiseZadanie/Write_to_file_txt/Delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_manager
+{
+   public class Delete
+    {
+        IDelete delete;
+        public Delete(IDelete delete)
+        {
+            this.delete = delete;
+        }
+
+        public void DeleteFile()
+        {
+            delete.DeleteFile();
+        }
+    }
+}
