523f297e5d16c286f53a1ffcbaac683d5921d207
Adam <adagor00673@gmail.com>
2022-04-17 13:15:07 +0200

    Zaimplementowanie biblioteki do obsługi plików

---
+++ NetwiseZadanie/Write_to_file_txt/Read.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_manager
+{
+   public class Read
+    {
+        IRead read;
+        public Read(IRead read)
+        {
+            this.read = read;
+        }
+
+        public string ReadFile()
+        {
+            return read.ReadFile();
+        }
+    }
+}
